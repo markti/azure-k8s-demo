733a4c78714f7916f3d26d2c7e2a21b55a6dc1fe
markti <markti@outlook.com>
2023-10-16 12:18:05 -0400

    add code

---
+++ src/terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "application_name" {
+  type = string
+}
+variable "environment_name" {
+  type = string
+}
+variable "primary_region" {
+  type = string
+}
+variable "domain_name" {
+  type = string
+}
+variable "vnet_cidr_block" {
+  type = string
+}
+variable "az_count" {
+  type = number
+}
